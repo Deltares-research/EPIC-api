eabb5164301c5c277a972746d7de0596b8bce430
Carles S. Soriano Pérez <carles.sorianoperez@deltares.nl>
2022-09-08 17:25:54 +0200

    test: Fixed failing tests

--- epic_app/tests/externals/test_eram_visuals_wrapper.py
+++ epic_app/tests/externals/test_eram_visuals_wrapper.py
@@ -1,21 +1,24 @@
+from epic_app.externals.eram_visuals_wrapper import EramVisualsWrapper
+from epic_app.externals.external_wrapper_base import ExternalWrapperStatusType
 from epic_app.tests import test_data_dir
-from epic_app.tests.epic_db_fixture import epic_test_db
 
 
 class TestSummaryEvolutionGraph:
-    def test_execute_r_snippet(self):
+    def test_execute_r_snippet_with_sample_data_succeeds(self):
         # 1. Define test data.
         _csv_file = test_data_dir / "csv" / "evo_summary.csv"
-        assert _csv_file.is_file()
+        assert _csv_file.exists()
         _png_file = test_data_dir / "test_evo_summary.png"
         if _png_file.is_file():
             _png_file.unlink()
-        if _png_file.with_suffix(".pdf").exists():
-            _png_file.unlink()
+        if _png_file.with_suffix(".pdf").is_file():
+            _png_file.with_suffix(".pdf").unlink()
 
         # 2. Run test.
-        # SummaryEvolutionGraph.execute_r_snippet(_csv_file, _png_file)
+        eram_visuals = EramVisualsWrapper()
+        eram_visuals.execute(dict(input_file=_csv_file, output_file=_png_file))
 
         # 3. Verify final expectations.
+        assert eram_visuals.status.status_type == ExternalWrapperStatusType.SUCCEEDED
         assert _png_file.exists()
         assert _png_file.with_suffix(".pdf").exists()
